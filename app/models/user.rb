2e665c14436ec7c65f9e27907aee572bd566519e
Michael Berhane <michael@michaelberhane.org>
2014-10-25 13:38:36 +0100

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  before_create :create_remember_token
   before_save { self.email = email.downcase }
   has_secure_password
 
@@ -8,5 +8,21 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+    # Create the token
+  end
 
 end
