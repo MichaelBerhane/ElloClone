5c553640cc9b59bcf8884d99c5b4707b9207a42e
Michael Berhane <michael@michaelberhane.org>
2014-10-28 16:02:57 +0000

    more auth work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token
+
   before_create :create_remember_token
   before_save { self.email = email.downcase }
   has_secure_password
@@ -15,7 +18,9 @@ class User < ActiveRecord::Base
   end
 
   def User.digest(token)
-    Digest::SHA1.hexdigest(token.to_s)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
   private
@@ -24,5 +29,24 @@ class User < ActiveRecord::Base
     self.remember_token = User.digest(User.new_remember_token)
     # Create the token
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 
 end
